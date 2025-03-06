e3fa09906e6d43060dcecbf56921fcbf1d5fcc2f
jaimezzapata <zapataval2304@gmail.com>
2025-03-06 08:58:26 -0500

    Se finaliza componente card

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+  return (
+    <section className="panel-control-card">
+      <div></div>
+      <div>
+        <p>Ver 1</p>
+      </div>
+    </section>
+  )
+}
+export default Card
--- src/Contenido.jsx
+++ src/Contenido.jsx
@@ -1,24 +1,9 @@
+import Card from "./Card"
+
 function Contenido() {
   return (
     <div className="panel-control-contenido">
-      <section className="panel-control-card">
-        <div></div>
-        <div>
-          <p>Ver 1</p>
-        </div>
-      </section>
-      <section className="panel-control-card">
-        <div></div>
-        <div>
-          <p>Ver 2</p>
-        </div>
-      </section>
-      <section className="panel-control-card">
-        <div></div>
-        <div>
-          <p>Ver 3</p>
-        </div>
-      </section>
+      <Card />
     </div>
   )
 }
