36e51f3df290ac226dedee7a28e3d7a685b6bef2
jaimezzapata <zapataval2304@gmail.com>
2025-03-06 08:28:38 -0500

    Se crea componente contendo

---
+++ src/Contenido.jsx
@@ -0,0 +1,25 @@
+function Contenido() {
+  return (
+    <div className="panel-control-contenido">
+      <section className="panel-control-card">
+        <div></div>
+        <div>
+          <p>Ver 1</p>
+        </div>
+      </section>
+      <section className="panel-control-card">
+        <div></div>
+        <div>
+          <p>Ver 2</p>
+        </div>
+      </section>
+      <section className="panel-control-card">
+        <div></div>
+        <div>
+          <p>Ver 3</p>
+        </div>
+      </section>
+    </div>
+  )
+}
+export default Contenido
--- src/PanelControl.jsx
+++ src/PanelControl.jsx
@@ -1,41 +1,11 @@
-import imgUser from './assets/user.jpg'
+import Contenido from "./Contenido"
+import MenuLateral from "./MenuLateral"
 
 function PanelControl() {
   return (
     <section className="panel-control">
-      <aside className="panel-control-aside">
-        <div>
-          <img src={imgUser} alt="Logo" />
-        </div>
-        <nav>
-          <ul>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-          </ul>
-        </nav>
-      </aside>
-      <div className="panel-control-contenido">
-        <section className="panel-control-card">
-          <div></div>
-          <div>
-            <p>Ver</p>
-          </div>
-        </section>
-        <section className="panel-control-card">
-          <div></div>
-          <div>
-            <p>Ver</p>
-          </div>
-        </section>
-        <section className="panel-control-card">
-          <div></div>
-          <div>
-            <p>Ver</p>
-          </div>
-        </section>
-      </div>
+      <MenuLateral />
+      <Contenido />
     </section>
   )
 }
